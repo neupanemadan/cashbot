aa9eff468aff54c32bd05853ebb003ecbc2762e9
Your Name <you@example.com>
2023-04-29 18:51:31 +0900

    minor fixes

--- app.py
+++ app.py
@@ -1,38 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 import requests
 
 app = Flask(__name__)
 API_KEY = '6V6HGEJIKYG0NW4H.'
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def home():
-	if request.method == 'POST':
-		try:
-			amount = request.form['amount']
-			amount = float(amount)
-			from_c = request.form['from_c']
-			to_c = request.form['to_c']
-			url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={}&to_currency={}&apikey={}'.format(
-				from_c, to_c, API_KEY)
-			response = requests.get(url=url).json()
-			rate = response['Realtime Currency Exchange Rate']['5. Exchange Rate']
-			rate = float(rate)
-			result = rate * amount
-			from_c_code = response['Realtime Currency Exchange Rate']['1. From_Currency Code']
-			from_c_name = response['Realtime Currency Exchange Rate']['2. From_Currency Name']
-			to_c_code = response['Realtime Currency Exchange Rate']['3. To_Currency Code']
-			to_c_name = response['Realtime Currency Exchange Rate']['4. To_Currency Name']
-			time = response['Realtime Currency Exchange Rate']['6. Last Refreshed']
-			
-			return render_template('home.html', result=round(result, 2), amount=amount,
-								from_c_code=from_c_code, from_c_name=from_c_name,
-								to_c_code=to_c_code, to_c_name=to_c_name, time=time)
-		except Exception as e:
-			return '<h1>Bad Request : {}</h1>'.format(e)
-
-	else:
-		return render_template('home.html')
+    try:
+        data = request.get_json()
+        amount = data['queryResult']['parameters']['unit-currency']['amount']
+        amount = int(amount)
+        from_c = data['queryResult']['parameters']['unit-currency']['currency']
+        to_c = data['queryResult']['parameters']['currency-name']
+        url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={}&to_currency={}&apikey={}'.format(
+            from_c, to_c, API_KEY)
+        response = requests.get(url=url).json()
+        rate = response['Realtime Currency Exchange Rate']['5. Exchange Rate']
+        rate = float(rate)
+        result = rate * amount
+        time = response['Realtime Currency Exchange Rate']['6. Last Refreshed']
+        
+        return f"{amount} {from_c} is equal to {result} {to_c} as per {time}"
+    except Exception as e:
+        return 'Sorry, Could not convert'
 
 
 if __name__ == "__main__":
